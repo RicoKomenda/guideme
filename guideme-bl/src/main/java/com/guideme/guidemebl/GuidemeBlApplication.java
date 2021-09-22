1be642539e8112fbac6d680541fd818e37de89d8
sornalingam <umapragash.sornalingam@adesso.de>
2021-09-22 15:12:42 +0200

    added initial project for backend

---
+++ guideme-bl/src/main/java/com/guideme/guidemebl/GuidemeBlApplication.java
@@ -0,0 +1,13 @@
+package com.guideme.guidemebl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuidemeBlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuidemeBlApplication.class, args);
+	}
+
+}
---
+++ guideme-bl/src/test/java/com/guideme/guidemebl/GuidemeBlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guideme.guidemebl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuidemeBlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
